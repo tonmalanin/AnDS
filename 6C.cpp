0c0e073e58ff685828e3ce04823e027470e7ff64
Маланьин Антон Олегович <122722613+tonmalanin@users.noreply.github.com>
2024-04-18 15:16:26 +0300

    Update 6C.cpp

--- 6C.cpp
+++ 6C.cpp
@@ -3,52 +3,52 @@
 
 int Log(int num) {
   int cnt = 1;
-  int lg = 0;
+  int log = 0;
   while (cnt < num) {
     cnt *= 2;
-    ++lg;
+    ++log;
   }
-  return lg;
+  return log;
 }
 
 int main() {
-  int max_h;
-  int pl_n;
-  std::cin >> max_h >> pl_n;
-  int lg = Log(max_h);
-  if (pl_n >= lg) {
-    std::cout << lg;
+  int max_hgt;
+  int pl_num;
+  std::cin >> max_hgt >> pl_num;
+  int log = Log(max_hgt);
+  if (pl_num >= log) {
+    std::cout << log;
     return 0;
   }
-  if (pl_n == 0) {
+  if (pl_num == 0) {
     std::cout << -1;
     return 0;
   }
-  std::vector<std::vector<int>> dp(pl_n, std::vector<int>(max_h + 1));
-  for (int i = 1; i <= max_h; ++i) {
+  std::vector<std::vector<int>> dp(pl_num, std::vector<int>(max_hgt + 1));
+  for (int i = 1; i <= max_hgt; ++i) {
     dp[0][i] = i - 1;
   }
-  for (int i = 0; i < pl_n; ++i) {
+  for (int i = 0; i < pl_num; ++i) {
     dp[i][1] = 0;
   }
-  for (int i = 1; i < pl_n; ++i) {
-    for (int j = 2; j <= max_h; ++j) {
-      int lp = 1;
-      int rp = j;
-      while (rp - lp > 1) {
-        int md = (lp + rp) / 2;
-        if (dp[i - 1][md] <= dp[i][j - md]) {
-          lp = md;
+  for (int i = 1; i < pl_num; ++i) {
+    for (int j = 2; j <= max_hgt; ++j) {
+      int bot_lim = 1;
+      int up_lim = j;
+      while (up_lim - bot_lim > 1) {
+        int curr = (bot_lim + up_lim) / 2;
+        if (dp[i - 1][curr] <= dp[i][j - curr]) {
+          bot_lim = curr;
         } else {
-          rp = md;
+          up_lim = curr;
         }
       }
-      dp[i][j] = std::max(dp[i - 1][lp], dp[i][j - lp]);
-      if (rp != j) {
-        dp[i][j] = std::min(std::max(dp[i - 1][rp], dp[i][j - rp]), dp[i][j]);
+      dp[i][j] = std::max(dp[i - 1][bot_lim], dp[i][j - bot_lim]);
+      if (up_lim != j) {
+        dp[i][j] = std::min(std::max(dp[i - 1][up_lim], dp[i][j - up_lim]), dp[i][j]);
       }
       ++dp[i][j];
     }
   }
-  std::cout << dp[pl_n - 1][max_h];
+  std::cout << dp[pl_num - 1][max_hgt];
 }
