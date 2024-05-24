cb5d33c6350d899e09575208ce7d10723a9faaf6
Маланьин Антон Олегович <122722613+tonmalanin@users.noreply.github.com>
2024-05-24 19:34:23 +0300

    Update 6K.cpp

--- 6K.cpp
+++ 6K.cpp
@@ -1,55 +1,54 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-int package_solving(std::vector<std::vector<int>>& dp,
-                    std::vector<int>& req_source, std::vector<int>& reward) {
-  int tasks_num = reward.size();
-  int source_num = dp[0].size() - 1;
-  for (int i = 1; i <= tasks_num; ++i) {
-    for (int j = 1; j <= source_num; ++j) {
+
+void package_solving(std::vector<int>& answer, std::vector<int>& weight,
+                     std::vector<int>& cost, int volume) {
+  int number = cost.size();
+  std::vector<std::vector<int>> dp(number + 1, std::vector<int>(volume + 1, 0));
+  for (int i = 1; i <= number; ++i) {
+    for (int j = 1; j <= volume; ++j) {
       dp[i][j] = dp[i - 1][j];
-      if (j >= req_source[i - 1] and
-          dp[i - 1][j - req_source[i - 1]] + reward[i - 1] > dp[i][j]) {
-        dp[i][j] = dp[i - 1][j - req_source[i - 1]] + reward[i - 1];
+      if (j >= weight[i - 1] and
+          dp[i - 1][j - weight[i - 1]] + cost[i - 1] > dp[i][j]) {
+        dp[i][j] = dp[i - 1][j - weight[i - 1]] + cost[i - 1];
       }
     }
   }
-  int ans = 0;
-  int curr_source_waste = 0;
-  for (int i = 0; i <= source_num; ++i) {
-    if (dp[tasks_num][i] > ans) {
-      ans = dp[tasks_num][i];
-      curr_source_waste = i;
+  int max_cost = 0;
+  int optimal_weight = 0;
+  for (int i = 0; i <= volume; ++i) {
+    if (dp[number][i] > max_cost) {
+      max_cost = dp[number][i];
+      optimal_weight = i;
     }
   }
-  return curr_source_waste;
+  for (int i = number; i > 0; --i) {
+    if (optimal_weight >= weight[i - 1] and
+        dp[i][optimal_weight] ==
+            dp[i - 1][optimal_weight - weight[i - 1]] + cost[i - 1]) {
+      answer.push_back(i);
+      optimal_weight -= weight[i - 1];
+    }
+  }
+  std::reverse(answer.begin(), answer.end());
 }
+
 int main() {
-  int tasks_num;
-  int source_num;
-  std::cin >> tasks_num >> source_num;
-  std::vector<int> req_source(tasks_num);
-  for (int i = 0; i < tasks_num; ++i) {
-    std::cin >> req_source[i];
+  int number;
+  int volume;
+  std::cin >> number >> volume;
+  std::vector<int> weight(number);
+  for (int i = 0; i < number; ++i) {
+    std::cin >> weight[i];
   }
-  std::vector<int> reward(tasks_num);
-  for (int i = 0; i < tasks_num; ++i) {
-    std::cin >> reward[i];
+  std::vector<int> cost(number);
+  for (int i = 0; i < number; ++i) {
+    std::cin >> cost[i];
   }
-  std::vector<std::vector<int>> dp(tasks_num + 1,
-                                   std::vector<int>(source_num + 1, 0));
-  int curr_source_waste = package_solving(dp, req_source, reward);
-  std::vector<int> tasks_id;
-  for (int i = tasks_num; i > 0; --i) {
-    if (curr_source_waste >= req_source[i - 1] and
-        dp[i][curr_source_waste] ==
-            dp[i - 1][curr_source_waste - req_source[i - 1]] + reward[i - 1]) {
-      tasks_id.push_back(i);
-      curr_source_waste -= req_source[i - 1];
-    }
-  }
-  std::reverse(tasks_id.begin(), tasks_id.end());
-  for (auto item : tasks_id) {
-    std::cout << item << std::endl;
+  std::vector<int> answer;
+  package_solving(answer, weight, cost, volume);
+  for (auto index : answer) {
+    std::cout << index << std::endl;
   }
 }
