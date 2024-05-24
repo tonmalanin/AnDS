3900bfbcb1460e15d3dc77962d943199b2da5d64
Маланьин Антон Олегович <122722613+tonmalanin@users.noreply.github.com>
2024-05-24 19:08:41 +0300

    Update 6C.cpp

--- 6C.cpp
+++ 6C.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
 #include <vector>
 
-int Log(int num) {
-  int cnt = 1;
+int Log(int number) {
+  int counter = 1;
   int log = 0;
-  while (cnt < num) {
-    cnt *= 2;
+  while (counter < number) {
+    counter *= 2;
     ++log;
   }
   return log;
 }
 
 int main() {
-  int max_hgt;
-  int pl_num;
-  std::cin >> max_hgt >> pl_num;
-  int log = Log(max_hgt);
-  if (pl_num >= log) {
+  int max_height;
+  int planes_number;
+  std::cin >> max_height >> planes_number;
+  int log = Log(max_height);
+  if (planes_number >= log) {
     std::cout << log;
     return 0;
   }
-  if (pl_num == 0) {
+  if (planes_number == 0) {
     std::cout << -1;
     return 0;
   }
-  std::vector<std::vector<int>> dp(pl_num, std::vector<int>(max_hgt + 1));
-  for (int i = 1; i <= max_hgt; ++i) {
+  std::vector<std::vector<int>> dp(planes_number, std::vector<int>(max_height + 1));
+  for (int i = 1; i <= max_height; ++i) {
     dp[0][i] = i - 1;
   }
-  for (int i = 0; i < pl_num; ++i) {
+  for (int i = 0; i < planes_number; ++i) {
     dp[i][1] = 0;
   }
-  for (int i = 1; i < pl_num; ++i) {
-    for (int j = 2; j <= max_hgt; ++j) {
-      int bot_lim = 1;
-      int up_lim = j;
-      while (up_lim - bot_lim > 1) {
-        int curr = (bot_lim + up_lim) / 2;
-        if (dp[i - 1][curr] <= dp[i][j - curr]) {
-          bot_lim = curr;
+  for (int i = 1; i < planes_number; ++i) {
+    for (int j = 2; j <= max_height; ++j) {
+      int l = 1;
+      int r = j;
+      while (r - l > 1) {
+        int md = (l + r) / 2;
+        if (dp[i - 1][md] <= dp[i][j - md]) {
+          l = md;
         } else {
-          up_lim = curr;
+          r = md;
         }
       }
-      dp[i][j] = std::max(dp[i - 1][bot_lim], dp[i][j - bot_lim]);
-      if (up_lim != j) {
-        dp[i][j] = std::min(std::max(dp[i - 1][up_lim], dp[i][j - up_lim]), dp[i][j]);
+      dp[i][j] = std::max(dp[i - 1][l], dp[i][j - l]);
+      if (r != j) {
+        dp[i][j] = std::min(std::max(dp[i - 1][r], dp[i][j - r]), dp[i][j]);
       }
       ++dp[i][j];
     }
   }
-  std::cout << dp[pl_num - 1][max_hgt];
+  std::cout << dp[planes_number - 1][max_height];
 }
