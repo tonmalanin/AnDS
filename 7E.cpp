52aea9ab19677f541a9503189ce06c359e935f0a
Маланьин Антон Олегович <122722613+tonmalanin@users.noreply.github.com>
2024-05-24 19:53:35 +0300

    Update 7E.cpp

--- 7E.cpp
+++ 7E.cpp
@@ -3,39 +3,43 @@
 #include <vector>
 
 struct Edge {
-  int dest;
-  int num;
+  int destination;
+  int index;
 };
 
-struct Graph {
-  int vert_num;
-  std::vector<std::vector<Edge>> adj_list;
-  Graph(int vert_num, std::vector<std::pair<int, int>>& edge_list)
-      : vert_num(vert_num) {
-    adj_list.resize(vert_num);
+class Graph {
+ private:
+  int vertex_number;
+  std::vector<std::vector<Edge>> adjacency_list;
+
+ public:
+  Graph(int vertex_number, std::vector<std::pair<int, int>>& edge_list)
+      : vertex_number(vertex_number) {
+    adjacency_list.resize(vertex_number);
     int counter = 0;
     for (auto pair : edge_list) {
       int start = pair.first;
-      int fin = pair.second;
-      adj_list[start - 1].push_back({fin - 1, counter + 1});
-      adj_list[fin - 1].push_back({start - 1, counter + 1});
+      int finish = pair.second;
+      adjacency_list[start - 1].push_back({finish - 1, counter + 1});
+      adjacency_list[finish - 1].push_back({start - 1, counter + 1});
       ++counter;
     }
   }
 
-  int Dfs(int curr_vert, std::vector<int>& depth, std::vector<int>& ans,
-          int curr_depth, int parent) {
-    depth[curr_vert] = curr_depth;
-    int highest_reachable = curr_depth;
-    for (auto vert : adj_list[curr_vert]) {
-      int dest = vert.dest;
-      int num = vert.num;
-      if (depth[dest] != -1 and dest != parent) {
-        highest_reachable = std::min(highest_reachable, depth[dest]);
-      } else if (depth[dest] == -1) {
-        int subtree_result = Dfs(dest, depth, ans, curr_depth + 1, curr_vert);
-        if (subtree_result > curr_depth) {
-          ans.push_back(num);
+  int Dfs(int current_vertex, std::vector<int>& depth, std::vector<int>& answer,
+          int current_depth, int parent) {
+    depth[current_vertex] = current_depth;
+    int highest_reachable = current_depth;
+    for (auto vertex : adjacency_list[current_vertex]) {
+      int destination = vertex.destination;
+      int index = vertex.index;
+      if (depth[destination] != -1 and destination != parent) {
+        highest_reachable = std::min(highest_reachable, depth[destination]);
+      } else if (depth[destination] == -1) {
+        int subtree_result =
+            Dfs(destination, depth, answer, current_depth + 1, current_vertex);
+        if (subtree_result > current_depth) {
+          answer.push_back(index);
         } else {
           highest_reachable = std::min(highest_reachable, subtree_result);
         }
@@ -45,30 +49,30 @@ struct Graph {
   }
 
   std::vector<int> FindBridges() {
-    std::vector<int> ans;
-    std::vector<int> depth(vert_num, -1);
-    for (int i = 0; i < vert_num; ++i) {
+    std::vector<int> answer;
+    std::vector<int> depth(vertex_number, -1);
+    for (int i = 0; i < vertex_number; ++i) {
       if (depth[i] == -1) {
-        Dfs(i, depth, ans, 0, -1);
+        Dfs(i, depth, answer, 0, -1);
       }
     }
-    return ans;
+    return answer;
   }
 };
 
 int main() {
-  int vert_num;
-  int edge_num;
-  std::cin >> vert_num >> edge_num;
-  std::vector<std::pair<int, int>> edge_list(edge_num);
-  for (int i = 0; i < edge_num; ++i) {
+  int vertex_number;
+  int edge_number;
+  std::cin >> vertex_number >> edge_number;
+  std::vector<std::pair<int, int>> edge_list(edge_number);
+  for (int i = 0; i < edge_number; ++i) {
     std::cin >> edge_list[i].first >> edge_list[i].second;
   }
-  Graph graph(vert_num, edge_list);
-  std::vector<int> ans = graph.FindBridges();
-  std::sort(ans.begin(), ans.end());
-  std::cout << ans.size() << std::endl;
-  for (auto edge : ans) {
+  Graph graph(vertex_number, edge_list);
+  std::vector<int> answer = graph.FindBridges();
+  std::sort(answer.begin(), answer.end());
+  std::cout << answer.size() << std::endl;
+  for (auto edge : answer) {
     std::cout << edge << " ";
   }
 }
